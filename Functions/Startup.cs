dcd91d7ca94bc2feba40beddadcd34053df1c917
Rebekah Kirk <rebekah.kirk@srmukdev.com>
2021-10-11 15:53:24 +0100

    First commit solution temmplate

---
+++ Functions/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(MembershipSystem.Functions.Startup))]
+
+namespace MembershipSystem.Functions
+{
+    public class Startup : FunctionsStartup 
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddLogging();
+            builder.Services.AddAutoMapper(Assembly.GetAssembly(this.GetType()));
+        }
+    }
+}
